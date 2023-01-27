7f352d241bf527429ad14b02551b4fc9cb7e43b1
Łukasz Zawadzki <lzawadzki@ZIS.local>
2023-01-27 15:57:32 +0100

    done version

--- src/main/java/pl/edu/wszib/labjackson/Person.java
+++ src/main/java/pl/edu/wszib/labjackson/Person.java
@@ -3,6 +3,7 @@ package pl.edu.wszib.labjackson;
 //import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@JsonIgnoreProperties(ignoreUnknown = true)
+//Redone on a classes
 public record Person(
         String firstName,
         String lastName,
